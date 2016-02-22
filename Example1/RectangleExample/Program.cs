56dd5e960becd1f263087a53db2d47919d4a82dd
Gerry Said <gerrysaid@gmail.com>
2016-02-22 19:44:29 +0100

    Lesson 4
    Created class Classroom and created an application for Rectangle area

---
+++ Example1/RectangleExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle myR = new Rectangle(10, 10);
+            Console.WriteLine(myR.drawMe());
+            Console.ReadLine();
+        }
+    }
+}
