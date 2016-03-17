d3588972ccf20dd078c8bcde6180c6ccc4930527
Gerry Said <gerrysaid@gmail.com>
2016-03-17 19:34:50 +0100

    Worksheet 6 classes
    Classes and form

---
+++ Example1/Worksheet6/Heptagon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet6
+{
+    class Heptagon:Shape
+    {
+        double[] sides = new double[7];
+
+        public override double getArea()
+        {
+            return 7 / 4 * Math.Pow(sides[0], 2) * 1 / Math.Tan(180 / 7);
+        }
+
+        public override double getPerimeter()
+        {
+            return sides[0] * 7;
+        }
+
+    }
+}
