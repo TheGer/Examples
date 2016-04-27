3ebc7ad0729ac7d45b5925de1e80a23595036c4e
Gerry Said <gerrysaid@gmail.com>
2016-04-27 17:17:04 +0200

    Added codefirst linq example 12
    added linq example using sqlite.

--- Example1/Example12/Program.cs
+++ Example1/Example12/Program.cs
@@ -1,22 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Example12
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
+        
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.WriteLine("Hello");
+            RunExample();
+            Console.ReadKey();
+
+            /* Application.EnableVisualStyles();
+             Application.SetCompatibleTextRenderingDefault(false);
+             Application.Run(new Form1());*/
+        }
+
+        public static void RunExample()
+        {
+            using (var context = new ContactContext())
+            {
+                var contacts = from a in context.Contact
+                               where a.FirstName.StartsWith("G")
+                               orderby a.FirstName
+                               select a;
+
+                foreach (var c in contacts)
+                {
+                    Console.WriteLine(c.Id + " " + c.FirstName);
+                }
+            }
+
+            // ### Add a db record
+            
+            using (var context = new ContactContext())
+            {
+                context.Configuration.ValidateOnSaveEnabled = false;
+                context.Contact.Add(
+                    new Contact
+                    {
+                        FirstName = "Gerard",
+                        SecondName = "Said",
+                        TelNumber = "1234",
+                    });
+                context.SaveChanges();
+            }
+
+            
+        }
+
+
+    }
+
+    
+    public class Contact
+    {
+        [Column("id")]
+        [Key]
+        public long? Id { get; set; }
+
+       public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string TelNumber { get; set; }
+
+
+    }
+
+    class ContactContext : DbContext
+    {
+        public DbSet<Contact> Contact { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Contact>()
+        .Property(c => c.Id)
+        .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
         }
     }
+
+   
+    
+
 }
